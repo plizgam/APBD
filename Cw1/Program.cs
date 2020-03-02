e0ce7a1ced6daf4bbc7d812cdca5c381602718b0
unknown <milosz.plizga1@gmail.com>
2020-03-02 13:28:19 +0100

    init

---
+++ Cw1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("hello world!");
+
+            //int? tmp1 = null;
+            //double tmp2 = 2.0;
+
+            //string tmp3 = "aaaa";
+            //bool tmp4 = true;
+
+            //var tmp5 = "ala ma kota";
+            //Console.WriteLine($"{tmp3} {tmp4} {int1+int2});
+
+            var newperson = new Person { FirstName = "miłosz" };
+
+
+
+
+            var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
+
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var htmlContent = await response.Content.ReadAsStringAsync();
+
+                        var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                        var matches = regex.Matches(htmlContent);
+
+                        foreach (var item in matches)
+                        {
+                            Console.WriteLine(item.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
