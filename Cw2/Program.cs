e2d6b54dad65e105f7d8c15c413ca6b03ba86904
unknown <milosz.plizga1@gmail.com>
2020-03-09 10:48:56 +0100

    init new project Cw2

---
+++ Cw2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\Miły\Desktop\dane.csv";
+
+            var lines = File.ReadLines(path);
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            var today = DateTime.UtcNow;
+
+            Console.WriteLine(today);
+        }
+    }
+}
