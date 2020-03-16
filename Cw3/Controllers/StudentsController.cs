aef0c0a883a20cd7e7e1565d306e84cecbd205a6
Miłosz Pliżga <milosz.plizga1@gmail.com>
2020-03-16 16:16:43 +0100

    Init 1

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -3,17 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Cw3.Models;
+using Cw3.DAL;
 
 namespace Cw3.Controllers
 {
     [ApiController]
-    [Route("api/Students")]
+    [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet("getStudents")]
-        public string GetStudents()
+
+        public readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return "Nowak, Kowalski";
+            _dbService = dbService;
         }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            //Pobranie rekordu z bazy danych o podanym id.
+            var student = new Student { IdStudent = id };
+
+            //Aktualizacja rekordu
+            student.FirstName = "Weronika";
+
+            //Zapis SaveChanges()
+
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            //Usunięcie rekordu z bazy danych o podanym id.
+
+            //Zapis SaveChanges()
+
+            return Ok("Usuwanie dokończone");
+        }
+        
     }
 }
