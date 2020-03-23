8a3f09fa4ff393cece49d94193ab83130c770e78
Miłosz Pliżga <milosz.plizga1@gmail.com>
2020-03-23 18:14:16 +0100

    completed with cw4

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Cw3.Models;
 using Cw3.DAL;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace Cw3.Controllers
 {
@@ -12,7 +16,7 @@ namespace Cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-
+        public string ConnectionString = "Data source=db-mssql;Initial Catalog=s18536;Integrated Security=True";
         public readonly IDbService _dbService;
 
         public StudentsController(IDbService dbService)
@@ -21,9 +25,64 @@ namespace Cw3.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetStudents(string orderBy)
+        public IActionResult GetStudents()
         {
-            return Ok(_dbService.GetStudents());
+            var students = new List<Student>();
+            
+            using(var con = new SqlConnection(ConnectionString))
+            using(var command = new SqlCommand())
+            {
+                command.Connection = con;
+                command.CommandText =
+                    "SELECT Student.FirstName, Student.LastName, Student.BirthDate, Studies.Name, Enrollment.Semester FROM Student, Studies, Enrollment WHERE Student.IdEnrollment = Enrollment.IdEnrollment AND Enrollment.IdStudy = Studies.IdStudy";
+
+                con.Open();
+
+                var dr = command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    students.Add(new Student
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirtDate = Convert.ToDateTime(dr["BirthDate"].ToString()),
+                        StudyName = dr["Name"].ToString(),
+                        Semester = Convert.ToInt32(dr["Semester"].ToString())
+                    });
+                }
+
+            }
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSemestersFromStudentId(string id)
+        {
+            var enrollments = new List<Enrollment>();
+            using (var con = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = con;
+                command.CommandText = "SELECT Enrollment.Semester, Enrollment.StartDate, Studies.Name FROM Enrollment, Student, Studies WHERE Student.IdEnrollment = Enrollment.IdEnrollment AND Enrollment.IdStudy = Studies.IdStudy AND Student.IndexNumber=@idStudent";
+                command.Parameters.AddWithValue("idStudent", id);
+
+                con.Open();
+
+                var dr = command.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    enrollments.Add(new Enrollment
+                    {
+                        Semester = Convert.ToInt32(dr["Semester"].ToString()),
+                        StartDate = Convert.ToDateTime(dr["StartDate"].ToString()),
+                        StudyName = dr["Name"].ToString()
+                    });
+                }
+
+            }
+            return Ok(enrollments);
         }
 
 
